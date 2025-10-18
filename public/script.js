55ea0c842f0364117fdabef3ac87a8aa112f27fe
Tousend1000 <fabianprus123@gmail.com>
2025-10-18 13:58:21 +0200

    Finished home page. Added message box. Finished page 3.

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 let currentPage = 1;
+let hasShortenedLink = false;
 
 function showMessage(message, type, duration) {
     if (!type) type = 'success';
@@ -74,12 +75,65 @@ function switchToPage(page) {
     }
 }
 
+function enableButtons() {
+    const buttonRight = document.getElementById('btn-right');
+    const buttonLeft = document.getElementById('btn-left');
+
+    buttonLeft.disabled = false;
+    buttonLeft.classList.remove('btn-disabled');
+    buttonRight.disabled = false;
+    buttonRight.classList.remove('btn-disabled');
+}
+
 function shortenURL() {
-    // TODO: Add logic that posts link to server.
-    // TODO: Add on error switch to that page and send popup.
-    // TODO: Add logic that renders third page and switches to it.
-    // TODO: Make all buttons disabled after switching.
-    // TODO: Reenable buttons on error.
+    const redirectUrl = document.getElementById('url-input').value;
+    const alias = document.getElementById('alias-input').value;
+
+    fetch('/api/v1/shortener/shorten', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            redirectUrl: redirectUrl,
+            alias: alias,
+        }),
+    })
+        .then(async response => {
+            if (!response.ok) {
+                let message = 'An unknown error occurred. Please try again later.';
+                try {
+                    const errorData = await response.json();
+                    if (errorData.error) message = errorData.error;
+                } catch {}
+                showMessage(message, 'error', 3);
+                enableButtons();
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (!data.success) {
+                showMessage(data.error, 'error', 3);
+                enableButtons();
+                throw new Error('User input error.');
+            }
+
+            hasShortenedLink = true;
+
+            // Render third page
+            const finalAliasSpan = document.getElementById('final-alias');
+            finalAliasSpan.innerHTML = alias;
+
+            const trackingCodeSpan = document.getElementById('tracking-code');
+            trackingCodeSpan.innerHTML = data.trackingCode;
+
+            const buttonLeft = document.getElementById('btn-left');
+
+            buttonLeft.disabled = false;
+            buttonLeft.classList.remove('btn-disabled');
+
+            switchToPage(3);
+        });
 }
 
 addEventListener('DOMContentLoaded', event => {
@@ -87,6 +141,7 @@ addEventListener('DOMContentLoaded', event => {
         // "Preload" page to ensure smooth transition
         switchToPage(2);
         switchToPage(1);
+        switchToPage(3);
         document.querySelectorAll('.container-page').forEach(p => p.style.transition = 'all 0.5s');
     }, 50);
     const currentUrlContainers = document.getElementsByClassName('current-url');
@@ -101,14 +156,15 @@ addEventListener('DOMContentLoaded', event => {
 
     function nextPage() {
         currentPage += 1;
-        if (currentPage > 3) {
-            currentPage = 3;
-        } else if (currentPage === 3) {
+        if (currentPage > 3) currentPage = 3; 
+        
+        if (currentPage === 3 && !hasShortenedLink) {
             buttonRight.disabled = true;
             buttonLeft.disabled = true;
             shortenURL();
             return
         }
+
         switchToPage(currentPage);
     }
 
@@ -130,15 +186,15 @@ addEventListener('DOMContentLoaded', event => {
         try { 
             new URL(value);
             buttonLeft.disabled = true;
-            buttonLeft.classList.add('btn-disabled')
+            buttonLeft.classList.add('btn-disabled');
             buttonRight.disabled = false;
-            buttonRight.classList.remove('btn-disabled')
+            buttonRight.classList.remove('btn-disabled');
         }
         catch (error) {
             buttonLeft.disabled = true;
-            buttonLeft.classList.add('btn-disabled')
+            buttonLeft.classList.add('btn-disabled');
             buttonRight.disabled = true;
-            buttonRight.classList.add('btn-disabled')
+            buttonRight.classList.add('btn-disabled');
         }
     });
 
@@ -158,10 +214,10 @@ addEventListener('DOMContentLoaded', event => {
         const value = aliasInput.value;
         if (value) {
             buttonRight.disabled = false;
-            buttonRight.classList.remove('btn-disabled')
+            buttonRight.classList.remove('btn-disabled');
         } else {
             buttonRight.disabled = true;
-            buttonRight.classList.add('btn-disabled')
+            buttonRight.classList.add('btn-disabled');
         }
     });
 
@@ -172,4 +228,23 @@ addEventListener('DOMContentLoaded', event => {
             nextPage();
         }
     });
+
+
+    // Selectable
+    document.querySelectorAll('.selectable').forEach(el => {
+        el.addEventListener('click', e => {
+            const range = document.createRange();
+            range.selectNodeContents(el);
+            const sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+
+            // Copy to clipboard
+            try {
+                document.execCommand('copy');
+                alert('Successfully copied link to clipboard!');
+            } catch {}
+        });
+    });
+
 });
